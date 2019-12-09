144ce7cdb661bc208df7bc0f66e646c277bc8ade
Maxim <maxim.engel@gmx.de>
2019-12-09 15:27:35 +0100

    First object in the middle of the scene

--- src/capstan/Main.java
+++ src/capstan/Main.java
@@ -1,26 +1,42 @@
 package capstan;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.geometry.Point3D;
+import javafx.scene.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.PickResult;
+import javafx.scene.shape.Cylinder;
 import javafx.scene.shape.Sphere;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 
+/**
+ * @version 0.1
+ */
 public class Main extends Application {
+
+    private static final float SCENE_WIDTH = 800;
+    private static final float SCENE_HEIGHT = 800;
+    private Scene scene;
+    private PerspectiveCamera camera;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Sphere sphere = new Sphere(50);
+        Cylinder wheel = new Cylinder(100, 200);
 
+        Group group = new Group();
+        group.getChildren().add(wheel);
 
-//        primaryStage.setTitle("Hello World");
-//        primaryStage.setScene(new Scene(root, 300, 275));
-//        primaryStage.show();
-    }
+        scene = new Scene(group, SCENE_WIDTH, SCENE_HEIGHT);
+        camera = new PerspectiveCamera(true);
+        scene.setCamera(camera);
+        camera.translateZProperty().set(-1000);
+        camera.setNearClip(1);
+        camera.setFarClip(1000);
 
-
-    public static void main(String[] args) {
-        launch(args);
+        primaryStage.setTitle("Capstan");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
